18c9e145ee1922ef1542573a8555a3e99e17cda2
vito.dandrea <v.dandrea@h-digital.it>
2021-10-04 14:24:38 +0200

    detail

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,24 @@
+import { HEROES } from './../mock-heroes';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelect(hero: Hero){
+    this.selectedHero = hero;
+  }
+
+}
